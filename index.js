44a5d71084661c9b7b4aae29915baadc3c4e4739
egor066416 <066416@gmail.com>
2018-05-17 18:34:08 +0300

    dz60

--- index.js
+++ index.js
@@ -106,15 +106,18 @@ function whoWin() {
   gameOwer(str);
 }
 function gameOwer(str) {
-  if (str === "XXX") {
-    stat(1, 0);
-    confirmm(str[0]);
-    _whoseTurn.innerHTML = "Победил игрок X";
-  } else if (str === "OOO") {
-    confirmm(str[0]);
-    stat(0, 1);
-    _whoseTurn.innerHTML = "Победил игрок О";
-  }
+	setTimeout(() => {
+		
+		if (str === "XXX") {
+			stat(1, 0);
+			confirmm(str[0]);
+			_whoseTurn.innerHTML = "Победил игрок X";
+		} else if (str === "OOO") {
+			confirmm(str[0]);
+			stat(0, 1);
+			_whoseTurn.innerHTML = "Победил игрок О";
+		}
+	}, 100);
 }
 function confirmm(data) {
   if (confirm(`Победил ${data}.\nИграем еще?`)) {
